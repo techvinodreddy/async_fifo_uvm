4c6d692107382a9b6cb374a3831cc1c93197a308
techvinodreddy <vinodreddy9739@gmail.com>
2022-02-22 14:06:18 +0530

    Create README.md

--- test/virtual_sequences.sv
+++ test/virtual_sequences.sv
@@ -2,6 +2,7 @@ class virtual_base_sequences extends uvm_sequence #(uvm_sequence_item);
   `uvm_object_utils(virtual_base_sequences)
 
   write_sequencer wr_seqr;
+  read_sequencer  rd_seqr;
   virtual_sequencer v_seqr;
   environment_config env_cfg;
 
@@ -17,13 +18,18 @@ task virtual_base_sequences::body();
   if(!uvm_config_db #(environment_config)::get(null,get_full_name(),"environment_config",env_cfg))
     `uvm_fatal("CONFIG","cannot get() env_cfg from uvm_config_db. Have you set it?")
 
-//   wr_seqr = new[env_cfg.no_of_agents];
-
   assert($cast(v_seqr, m_sequencer))
 
   else
     `uvm_error("BODY", "Erroe in $cast of virtual sequencer")
 
-    wr_seqr = v_seqr.wr_seqr;
+    if(env_cfg.has_agt_top)
+    begin
+      if(env_cfg.has_wr_agt)
+        wr_seqr = v_seqr.wr_seqr;
+
+      if(env_cfg.has_rd_agt)
+        rd_seqr = v_seqr.rd_seqr;  
+    end
 
 endtask : body
